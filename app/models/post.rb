534f974554357916be8fd9638ae0fb942a14e854
MulloaV <mulloa0179@e-camp.cl>
2022-03-05 01:58:56 -0300

    Adds Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates_presence_of :title, :body
     validates_length_of :title, minimum:  5
     validates_length_of :body, maximum: 1000
+
+    belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-10.times do |x|
-    Post.create(title: "This is the post #{x}", body: "Can we seed the db with #{x} posts?")
+User.create(email: 'hola@gmail.com', password: '123456', password_confirmation: '123456')
+
+100.times do |x|
+    Post.create(title: "This is the post #{x}", body: "Can we seed the db with #{x} posts?", user_id: User.first.id)
 end 

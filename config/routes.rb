58a2275c5046c7abcbb28a041c9a1a78baa44fe9
MulloaV <mulloa0179@e-camp.cl>
2022-03-05 03:02:36 -0300

    More devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   resources :posts
   get 'home', to:   'pages#home'
   get 'about', to:  'pages#about'
 
   root "pages#home"
 end
+
